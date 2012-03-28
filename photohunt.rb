dbf8a16c15210209d7db8dc3ac96f47ac0054147
Russell Harmon <russ@eatnumber1.com>
2012-03-27 20:04:10 -0400

    Lots of updates. It should be usable now.

--- photohunt.rb
+++ photohunt.rb
@@ -10,9 +10,10 @@ require 'sequel'
 require 'zip/zipfilesystem'
 require 'exifr'
 
+
 require 'logger'
 
-#Sequel::Model.plugin :json_serializer
+GAME_ID = "3CF33F29-DE6C-4C9D-ACA7-71E37A2EDFBE"
 
 Sequel.inflections do |inflect|
 	inflect.irregular "bonus", "bonuses"
@@ -23,6 +24,8 @@ DB = Sequel.sqlite
 DB.logger = Logger.new($stdout)
 DB.sql_log_level = :debug
 
+Sequel::Model.plugin :json_serializer
+
 DB.transaction do
 	DB.create_table! :clues_tags do
 		foreign_key :clue_id, :clues, :null => false
@@ -30,7 +33,7 @@ DB.transaction do
 	end
 
 	DB.create_table! :bonus_completions do
-		foreign_key :clue_completion_id, :clue_completions
+		foreign_key :clue_completion_id, :clue_completions, :null => false
 		foreign_key :bonus_id, :bonuses, :null => false
 	end
 	
@@ -42,7 +45,7 @@ DB.transaction do
 
 	DB.create_table! :bonuses do
 		primary_key :id
-		foreign_key :clue_id, :clues
+		foreign_key :clue_id, :clues, :null => false
 		String :description, :null => false
 		Integer :points, :null => false
 	end
@@ -54,63 +57,49 @@ DB.transaction do
 
 	DB.create_table! :clues do
 		primary_key :id
-		foreign_key :game_id, :games, :null => false
+		foreign_key :game_id, :games, :null => false, :type => String
 		String :description, :null => false
 		Integer :points, :null => false
 	end
 
 	DB.create_table! :tokens do
 		primary_key :id
-		foreign_key :team_id, :teams
+		foreign_key :team_id, :teams, :null => false
 		String :token, :null => false
 	end
 
 	DB.create_table! :photos do
-		foreign_key :team_id, :teams
+		foreign_key :team_id, :teams, :null => false
 		String :guid, :null => false, :primary_key => true
 		File :data, :null => false
-		FalseClass :judge
-		String :notes, :text => true
+		FalseClass :judge, :null => true
+		String :notes, :text => true, :null => true
 		DateTime :submission, :default => "datetime('now','localtime')".lit
 	end
 	
 	DB.create_table! :teams do
 		primary_key :id
-		foreign_key :game_id, :games
+		foreign_key :game_id, :games, :null => false, :type => String
 		String :name, :null => false
 		FalseClass :finished, :null => false, :default => false
 	end
 
 	DB.create_table! :games do
-		primary_key :id
-		DateTime :start
-		DateTime :end
-		Integer :maxPhotos
-		Integer :maxJudgedPhotos
+		String :id, :primary_key => true
+		DateTime :start, :null => false
+		DateTime :end, :null => false
+		Integer :max_photos, :null => false
+		Integer :max_judged_photos, :null => false
 	end
 end
 
 class Bonus < Sequel::Model
-	def to_json(*opts)
-		{
-			:description => self.description,
-			:points => self.points
-		}.to_json(*opts)
-	end
 end
 
 class Clue < Sequel::Model
-	one_to_many :bonuses
+	one_to_many :bonuses, :order => :id
 	many_to_many :tags
-
-	def to_json(*opts)
-		{
-			:description => self.description,
-			:points => self.points,
-			:tags => self.tags,
-			:bonuses => self.bonuses
-		}.to_json(*opts)
-	end
+	many_to_one :game
 end
 
 class Tag < Sequel::Model
@@ -127,13 +116,14 @@ end
 
 class Photo < Sequel::Model
 	unrestrict_primary_key
-	one_to_many :clue_completions
+	one_to_many :clue_completions, :order => :clue_id
+	many_to_one :team
 end
 
 class ClueCompletion < Sequel::Model
 	many_to_one :photo
 	many_to_one :clue
-	one_to_many :bonus_completions
+	one_to_many :bonus_completions, :order => :bonus_id
 end
 
 class BonusCompletion < Sequel::Model
@@ -147,16 +137,19 @@ class Token < Sequel::Model
 end
 
 class Game < Sequel::Model
-	one_to_many :teams
-	one_to_many :clues
+	unrestrict_primary_key
+	one_to_many :teams, :order => :name
+	one_to_many :clues, :order => :id
 end
 
 DB.transaction do
+	# There can only be ONE of these, or it messes up the clue ID numbering
 	game = Game.create(
+		:id => GAME_ID,
 		:start => Time.at(0),
 		:end => Time.at(946702800),
-		:maxPhotos => 30,
-		:maxJudgedPhotos => 24
+		:max_photos => 30,
+		:max_judged_photos => 24
 	)
 	team = Team.new(
 		:name => "faggot",
@@ -168,17 +161,30 @@ DB.transaction do
 		:points => 100
 	)
 	game.add_clue(clue)
+	game.add_clue(
+		:description => "Your team in a bank vault.",
+		:points => 1000
+	)
+	clue3 = Clue.new(
+		:description => "Your team on a boat.",
+		:points => 5
+	)
+	game.add_clue(clue3)
+	clue3.add_bonus(
+		:description => "if it is in water",
+		:points => 10
+	)
+	clue3.add_bonus(
+		:description => "if it is a real boat",
+		:points => 10
+	)
 	clue.add_tag(:tag => "Location")
 	clue.add_tag(:tag => "Goose")
 	bonus = Bonus.new(
 		:description => "with a goose",
-		:points => 1000
+		:points => 50
 	)
 	clue.add_bonus(bonus)
-	Bonus.new(
-		:description => "with a goose",
-		:points => 1000
-	)
 	data = nil
 	guid = File.open("b") do |f|
 		data = f.read
@@ -202,6 +208,7 @@ module PhotohuntError
 	ERR_UNSPEC = { :code => 1, :message => "Unspecified error. Message contains additional information" }
 	ERR_NOTAUTH = { :code => 2, :message => "Authorization token invalid" }
 	ERR_GAMEOVER = { :code => 3, :message => "Photohunt competition is over. No judged submissions allowed" }
+	ERR_NOTFOUND = { :code => 4, :message => "Photo not found" }
 end
 
 def authenticate
@@ -227,22 +234,27 @@ put '/api/photos/new', :provides => 'json' do
 		)
 		team.add_photo(photo)
 	end
+	p team
 	return PhotohuntError::ERR_SUCCESS.merge({ :data => photo.guid }).to_json
 end
 
+# TODO: Validate that the bonuses passed for a particular clue are legal
+# bonuses for that clue.
 put '/api/photos/edit', :provides => 'json' do
 	pass unless request.accept? 'application/json'
-	authenticate
+	team = authenticate
 
 	data = JSON.parse(request.body.read)
-	# TODO: Handle calling edit on a photo that doesn't exist
-	# TODO: Add a find filter on the team_id
-	photo = Photo.find(:guid => params[:id])
-	photo.update(:judge => data["judge"], :notes => data["notes"])
-	data["clues"].each do |clue|
-		photo.add_clue_completion(:clue => Clue[clue["id"]], :bonus => nil)
-		clue["bonus_id"].each do |bonus_id|
-			photo.add_clue_completion(:clue => Clue[clue["id"]], :bonus => Bonus[bonus_id])
+	photo = Photo.find(:guid => params[:id], :team => team)
+	halt 404, PhotohuntError::ERR_NOTFOUND.merge({ :data => nil }).to_json if photo == nil
+	DB.transaction do
+		photo.update(:judge => data["judge"], :notes => data["notes"])
+		data["clues"].each do |clue|
+			clue_completion = ClueCompletion.new(:clue => Clue[clue["id"]])
+			photo.add_clue_completion(clue_completion)
+			clue["bonus_id"].each do |bonus_id|
+				clue_completion.add_bonus_completion(:bonus => Bonus[bonus_id])
+			end
 		end
 	end
 
@@ -251,7 +263,18 @@ end
 
 get '/api/clues', :provides => 'json' do
 	pass unless request.accept? 'application/json'
-	return PhotohuntError::ERR_SUCCESS.merge({ :data => Clue.all }).to_json
+	# TODO: Doing JSON.parse right after Clue.to_json is really inefficient
+	return PhotohuntError::ERR_SUCCESS.merge({ :data => JSON.parse(Clue.filter(:game => Game[GAME_ID]).to_json(
+		:naked => true,
+		:except => :game_id,
+		:include => {
+			:bonuses => {
+				:except => :clue_id,
+				:naked => true
+			},
+			:tags => {}
+		}
+	))}).to_json
 end
 
 get '/api/info', :provides => 'json' do
@@ -261,14 +284,32 @@ get '/api/info', :provides => 'json' do
 		:team => team.name,
 		:startTime => team.game.start,
 		:endTime => team.game.end,
-		:maxPhotos => team.game.maxPhotos,
-		:maxJudgedPhotos => team.game.maxJudgedPhotos
+		:max_photos => team.game.max_photos,
+		:max_judged_photos => team.game.max_judged_photos
 	}}).to_json
+end
+
+get '/clues', :provides => 'text' do
+	out = StringIO.new
+	out.printf("Clue sheet for Photo Hunt\n\n")
+	out.printf("%-20s %s\n", "Start Time: ", Game[GAME_ID].start)
+	out.printf("%-20s %s\n", "End Time: ", Game[GAME_ID].end)
+	out.printf("%-20s %d\n", "Max Photos: ", Game[GAME_ID].max_photos)
+	out.printf("%-20s %d\n", "Max Judged Photos: ", Game[GAME_ID].max_judged_photos)
+	out.printf("\n")
+	Game[GAME_ID].clues.each do |clue|
+		out.printf("%-4s\t%+5d\t%s\n", "#{clue.id}.", clue.points, clue.description)
+		clue.bonuses.each do |bonus|
+			out.printf("\t%+5d\t\t%s\n", bonus.points, bonus.description)
+		end
+	end
+	out.rewind
+	return out.read
 end
 
 # The zipfile library seems to create temporary working files in /tmp and
 # doesn't delete them. I can't help this
-get '/api/export.zip', :provides => 'zip' do
+get '/export.zip', :provides => 'zip' do
 	pass unless request.accept? 'application/zip'
 	# TODO: Add judges-only auth.
 
@@ -280,13 +321,13 @@ get '/api/export.zip', :provides => 'zip' do
 		dirbase = "photohunt"
 		zipfile.dir.mkdir(dirbase)
 
-		Team.each do |team|
+		Game[GAME_ID].teams.each do |team|
 			photoctr = 1
 			curdir = "#{dirbase}/#{team.name}"
 			zipfile.dir.mkdir(curdir)
 
 			zipfile.file.open("#{curdir}/#{team.name}.txt", "w") do |doc|
-				doc.puts "Team #{team.name}\n"
+				doc.puts "Team \"#{team.name}\"\n"
 
 				team.photos.each do |photo|
 					exposure = "unavailable"
@@ -305,12 +346,11 @@ get '/api/export.zip', :provides => 'zip' do
 						file.write(photo.data)
 					end
 
-					# TODO: Tell if it's late.
 					doc.printf("\n%d.\n", photoctr)
 					doc.printf("\tJudged: %s\n", photo.judge) unless photo.judge == nil
 					doc.printf("\tExposure Time: %s\n", exposure)
 					# TODO: Make sure updates don't change submission time.
-					doc.printf("\tSubmission Time: %s\n", photo.submission)
+					doc.printf("\tSubmission Time: %s %s\n", photo.submission, photo.submission > Game[GAME_ID].end ? "LATE" : "")
 					if photo.clue_completions.length != 0
 						points = 0
 						clue_str = StringIO.new
