0de83016b373670731b14f28368cca750335a637
Russell Harmon <russ@eatnumber1.com>
2012-04-11 18:48:06 -0400

    Add commented out mysql database.

--- lib/database.rb
+++ lib/database.rb
@@ -12,7 +12,9 @@ module Photohunt
 			inflect.irregular "bonus", "bonuses"
 		end
 
+		# SQLite suffers from a "database is locked" race condition.
 		DB = Sequel.connect("sqlite://sql/photohunt.sql")
+		#DB = Sequel.connect(:adapter => "mysql", :user => "photohunt", :host => "db.csh.rit.edu", :database => "photohunt", :password => "")
 		#DB = Sequel.sqlite
 		#DB.logger = Logger.new($stdout)
 		#DB.sql_log_level = :debug
