7ef5fead5c4227d898c5b575b1e9ec8cbebce104
Russell Harmon <russ@eatnumber1.com>
2012-04-11 01:24:10 -0400

    Allow updating of the database with yaml.rb.

--- lib/schema.rb
+++ lib/schema.rb
@@ -9,49 +9,49 @@ include Photohunt::Database
 module Photohunt
 	module Database
 		DB.transaction do
-			DB.create_table :clues_tags do
+			DB.create_table? :clues_tags do
 				foreign_key :clue_id, :clues, :null => false, :on_delete => :cascade
 				foreign_key :tag_id, :tags, :null => false, :on_delete => :cascade
 			end
 
-			DB.create_table :bonus_completions do
+			DB.create_table? :bonus_completions do
 				foreign_key :clue_completion_id, :clue_completions, :null => false, :on_delete => :cascade
 				foreign_key :bonus_id, :bonuses, :null => false, :on_delete => :cascade
 			end
 
-			DB.create_table :clue_completions do
+			DB.create_table? :clue_completions do
 				primary_key :id
 				foreign_key :photo_id, :photos, :null => false, :type => String, :on_delete => :cascade
 				foreign_key :clue_id, :clues, :null => false, :on_delete => :cascade
 			end
 
-			DB.create_table :bonuses do
+			DB.create_table? :bonuses do
 				primary_key :id
 				foreign_key :clue_id, :clues, :null => false, :on_delete => :cascade
 				String :description, :null => false
 				Integer :points, :null => false
 			end
 
-			DB.create_table :tags do
+			DB.create_table? :tags do
 				primary_key :id
 				String :tag, :unique => true, :null => false
 			end
 
-			DB.create_table :clues do
+			DB.create_table? :clues do
 				primary_key :id
 				foreign_key :game_id, :games, :null => false, :type => String, :on_delete => :cascade
 				String :description, :null => false
 				Integer :points, :null => false
 			end
 
-			DB.create_table :tokens do
+			DB.create_table? :tokens do
 				foreign_key :team_id, :teams, :null => false, :on_delete => :cascade
 				foreign_key :game_id, :games, :null => false, :type => String, :on_delete => :cascade
 				# Tokens must be globally unique.
 				String :token, :null => false, :primary_key => true
 			end
 
-			DB.create_table :photos do
+			DB.create_table? :photos do
 				foreign_key :team_id, :teams, :null => false, :on_delete => :cascade
 				String :guid, :null => false, :primary_key => true
 				File :data, :null => false
@@ -61,13 +61,13 @@ module Photohunt
 				DateTime :submission, :default => "datetime('now','localtime')".lit
 			end
 
-			DB.create_table :teams do
+			DB.create_table? :teams do
 				primary_key :id
 				foreign_key :game_id, :games, :null => false, :type => String, :on_delete => :cascade
 				String :name, :null => false
 			end
 
-			DB.create_table :games do
+			DB.create_table? :games do
 				String :id, :primary_key => true
 				DateTime :start, :null => false
 				DateTime :end, :null => false
@@ -75,7 +75,7 @@ module Photohunt
 				Integer :max_judged_photos, :null => false
 			end
 
-			DB.create_table :judges_tokens do
+			DB.create_table? :judges_tokens do
 				String :token, :null => false, :primary_key => true
 			end
 		end
