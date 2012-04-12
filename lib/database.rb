cd6f856b54e3c7585d5dfebdf6730b8a67c390e6
Russell Harmon <russ@eatnumber1.com>
2012-04-11 20:06:08 -0400

    Whoops, forgot to update the db adapter.

--- lib/database.rb
+++ lib/database.rb
@@ -15,7 +15,7 @@ module Photohunt
 
 		# SQLite suffers from a "database is locked" race condition.
 		DB = Sequel.connect("sqlite://sql/photohunt.sql")
-		#DB = Sequel.connect(:adapter => "mysql", :user => "photohunt", :host => "db.csh.rit.edu", :database => "photohunt", :password => "")
+		#DB = Sequel.connect(:adapter => "mysql2", :user => "photohunt", :host => "db.csh.rit.edu", :database => "photohunt", :password => "")
 		#DB = Sequel.sqlite
 		#DB.logger = Logger.new($stdout)
 		#DB.sql_log_level = :debug
