7f212063954113dd1ad24676be4cb1b28f0a8ff9
Russell Harmon <russ@eatnumber1.com>
2012-04-13 19:27:38 -0400

    Revert "Enable mysql2 reconnecting."
    This reverts commit 40b61e9ecdb8fad9ceff7af48218a074bd612b34.

--- lib/database.rb
+++ lib/database.rb
@@ -14,10 +14,6 @@ module Photohunt
 
 		$config = { :debug => false }.merge(YAML.load_file("config.yml"))
 		Bundler.require($config["database"]["adapter"])
-		# Enabling reconnect here can cause problems if we change the encoding
-		# (and maybe others). See
-		# https://groups.google.com/forum/?fromgroups#!topic/sequel-talk/wNoFul3dmEg
-		$config["database"]["after_connect"] = proc{ |c| c.reconnect = true } if $config["database"]["adapter"] == "mysql2"
 		DB = Sequel.connect($config["database"])
 		module DBConn
 			def reconnect
